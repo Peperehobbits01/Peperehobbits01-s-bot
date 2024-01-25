cfee06d8e96b525eadd10b1148553c22c8b80603
Peperehobbit01 <contact.peperehobbits01@gmail.com>
2024-01-25 12:26:34 +0100

    Mise à jour de guildMemberRemove.js
    Mise à jour d'un évènement et retrait des fichier guildMemberAdd.js , guildMemberRemove.js et voiceStateUpdate.js qui se trouve dans la directory parent.

--- Events/guildMemberRemove.js
+++ Events/guildMemberRemove.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js")
 
-module.exports = async (bot, member) => {
+module.exports = async (bot, user) => {
 
     const logsChannel = "931457930660835333"
 
     const removeMember = new Discord.EmbedBuilder()
         .setColor(bot.color)
         .setTitle("Un membre a quittée le serveur")
-        .setDescription(`Le membre ${member.username} , ${member.tag}\nID ${member.id}`)
+        .setDescription(`Le membre ${user.username} , ${user.tag}\nID ${user.id}`)
         .setFooter({ text: "Gérée par l'instance de Peperehobbits01's Bot", iconURL: bot.user.displayAvatarURL({ dynamic: true }) })
 
         logsChannel.send({ embeds: [removeMember] })
