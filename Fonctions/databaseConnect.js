dc502e3242538ebf4f60d265e75443996c9d9a09
Peperehobbits01 <peperehobbits01@proton.me>
2026-05-03 17:22:50 +0200

    Refactor: Use environment variables for DB credentials and refine query execution logic

--- Fonctions/databaseConnect.js
+++ Fonctions/databaseConnect.js
@@ -1,11 +1,11 @@
 const mysql = require(`mysql2/promise`);
 
 const databaseConfig = {
-    connectionLimit: "10",
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "peperehobbits01's bot",
+    connectionLimit: process.env.DB_CONNECTION_LIMIT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 };
 
 const pool = mysql.createPool(databaseConfig);
@@ -14,12 +14,21 @@ async function executeQuery(query) {
     let connection;
     try {
         connection = await pool.getConnection();
-        const [rows] = await connection.execute(query);
+        let sql, values;
+
+        if (Array.isArray(query) && query.length === 2) {
+            [sql, values] = query;
+        } else {
+            sql = query;
+            values = undefined;
+        }
+
+        const [rows] = await connection.execute(sql, values);
         return rows;
     } catch (error) {
         console.error(`Erreur lors de l'exécution de la requête: \n`, error);
     } finally {
-        if (!connection) return connection.release();
+        if (connection) connection.release();
     }
 }
 
