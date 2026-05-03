ed3bf14f22154591415be86b4548d850b19f2587
Peperehobbits01 <peperehobbits01@proton.me>
2026-05-03 17:59:07 +0200

    Fix: user is no longer undefined.

--- Events/guildMemberRemove.js
+++ Events/guildMemberRemove.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js")
 
 module.exports = async (bot, member) => {
 
-    const logsChannel = member.guild.channels.cache.get("1153677499771928596")
+    const logsChannel = process.env.LOGS_CHANNEL_MEMBER;
 
     const removeMember = new Discord.EmbedBuilder()
         .setColor(process.env.BOT_COLOR)
         .setTitle("Un membre a quittée le serveur")
-        .setDescription(`Le membre ${member.username} , ${member.user.tag}\nID ${user.id}`)
+        .setDescription(`Le membre ${member.username} , ${member.user.tag}\nID ${member.id}`)
         .setFooter({ text: "Gérée par l'instance de Peperehobbits01's Bot", iconURL: bot.user.displayAvatarURL({ dynamic: true }) })
 
         logsChannel.send({ embeds: [removeMember] })
