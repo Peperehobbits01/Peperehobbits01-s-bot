8fea59669125bcc5b0e3cf39452cfc87ff55c19f
Peperehobbits01 <peperehobbits01@proton.me>
2026-05-08 12:23:52 +0200

    Update member join/leave logging embeds and channel retrieval

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -2,16 +2,17 @@ const Discord = require("discord.js");
 
 module.exports = async (bot, member) => {
 
-    if(member.guild.id !== ("931457929431908373")) return;
-    bot.channels.cache.get("931457930325270614").send(`Bienvenue à <@${member.id}>, il vient d'arrivée sur le serveur!`)
+    const welcomeChannel = member.guild.channels.cache.get("931457930325270614")
+    welcomeChannel.send(`Bienvenue à ${member}, il vient d'arrivée sur le serveur!`)
 
-    const logsChannel = member.guild.channels.cache.get("1153677499771928596")
+    const logsChannel = member.guild.channels.cache.get(process.env.LOGS_CHANNEL_MEMBER)
 
     const logsNewMember = new Discord.EmbedBuilder()
-    .setColor(process.env.BOT_COLOR)
-    .setTitle("Nouveau membre sur le serveur")
-    .setDescription(`${member.user.tag} a rejoint le serveur.`)
-    .setFooter({ text: "Gérée par l'instance de Peperehobbits01's Bot", iconURL: bot.user.displayAvatarURL({ dynamic: true }) })
+        .setColor(process.env.BOT_COLOR)
+        .setAuthor({name: member.displayName, iconURL: member.displayAvatarURL({dynamic : true})})
+        .setDescription(`${member} a rejoint le serveur.\nUtilisateur : ${member.name}\n**ID** :\nUtilisateur : ${member.id}`)
+        .setFooter({ text: "Gérée par l'instance de Peperehobbits01's Bot", iconURL: bot.user.displayAvatarURL({ dynamic: true }) })
+        .setTimestamp()
 
-    logsChannel.send({ embeds: [logsNewMember] })
+    await logsChannel.send({ embeds: [logsNewMember] })
 }
