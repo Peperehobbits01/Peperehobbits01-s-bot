3ebfcef9bcda4d55b9b677b015fe0427b63f4c1a
Peperehobbits01 <peperehobbits01@proton.me>
2026-06-02 18:28:54 +0200

    Feat: Log message updates

--- Events/messageUpdate.js
+++ Events/messageUpdate.js
@@ -1,7 +1,32 @@
 const Discord = require("discord.js")
 
-module.exports = async(bot, message, oldMessage, NewMessage) => {
+module.exports = async(bot, message, oldMessage) => {
 
-    const logsChannel = "931457930660835333"
+    if(message.partial) return;
+    const logsChannel = message.guild.channels.cache.get(process.env.LOGS_CHANNEL_MESSAGE)
+    const fetchedLogs = await message.guild.fetchAuditLogs({
+        type: Discord.AuditLogEvent.messageUpdate,
+        limit: 1,
+    });
 
+    const channelLog = fetchedLogs.entries.find(entry =>
+        entry.target?.id === message.id || entry.target?.id === message.author.id
+    );
+
+    const executor = channelLog?.executor || message.author;
+
+    const messageUpdateEmbed = new Discord.EmbedBuilder()
+        .setAuthor({
+            name: executor.displayName,
+            iconURL: executor.displayAvatarURL({dynamic: true})
+        })
+        .setColor(process.env.BOT_COLOR)
+        .setDescription(`Un message a été modifiée par ${executor}\n\nAncien message : ${message.content}\nNouveau message : ${oldMessage.content}\nAuteur : ${oldMessage.author.displayName}\nPar : ${executor.displayName}\n\n**ID** :\nAuteur : ${message.author.id}\nPar : ${executor.id}`)
+        .setFooter({
+            text: "Gérée par l'instance de Peperehobbits01's Bot",
+            iconURL: bot.user.displayAvatarURL({dynamic: true})
+        })
+        .setTimestamp()
+
+    await logsChannel.send({embeds: [messageUpdateEmbed]});
 }
