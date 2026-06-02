ad1a1836a36d7e2ae22aa1e7946cf82e8efd238a
Peperehobbits01 <peperehobbits01@proton.me>
2026-06-02 17:42:01 +0200

    Feat: Introducing messageDelete logging

---
+++ Events/messageDelete.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js")
+
+module.exports = async (bot, message) => {
+
+    if(message.partial) return;
+    const logsChannel = message.guild.channels.cache.get(process.env.LOGS_CHANNEL_MESSAGE);
+    const fetchedLogs = await message.guild.fetchAuditLogs({
+        type: Discord.AuditLogEvent.messageDelete,
+        limit: 1,
+    });
+
+    const channelLog = fetchedLogs.entries.find(entry =>
+        entry.target?.id === message.id || entry.target?.id === message.author.id
+    );
+
+    const executor = channelLog?.executor || message.author;
+
+    const MessageRemoveEmbed = new Discord.EmbedBuilder()
+        .setAuthor({
+            name: executor.displayName,
+            iconURL: executor.displayAvatarURL({dynamic: true})
+        })
+        .setColor(process.env.BOT_COLOR)
+        .setDescription(`Un message de ${message.author} a été supprimer par ${executor}\n\nMessage supprimer : ${message}\nAuteur : ${message.author.displayName}\nPar : ${executor.displayName}\n\n**ID** :\nAuteur : ${message.author.id}\nPar : ${executor.id}`)
+        .setFooter({
+            text: "Gérée par l'instance de Peperehobbits01's Bot",
+            iconURL: bot.user.displayAvatarURL({dynamic: true})
+        })
+        .setTimestamp()
+
+    await logsChannel.send({embeds: [MessageRemoveEmbed]})
+}
