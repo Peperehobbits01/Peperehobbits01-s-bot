9d8e9cee4f88dc53b33a7b6aeab5a2d2a594437c
Peperehobbits01 <peperehobbits01@proton.me>
2026-07-11 16:58:49 +0200

    Fixing an edge case where level would be undefined

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -7,31 +7,32 @@ module.exports = async(bot, message) => {
 
     const querySearch = `SELECT * FROM xp WHERE guild = '${message.guildId}' AND user = '${message.author.id}'`
     const results = await executeQuery(querySearch)
+    const xptogive = Math.floor(Math.random() * 30) + 15;
 
     if(results.length < 1) {
 
-        const queryAdd = `INSERT INTO xp (guild, user, xp, level) VALUES (${message.guildId}, '${message.author.id}', '0', '0')`
+        const queryAdd = `INSERT INTO xp (guild, user, xp, level) VALUES (${message.guildId}, '${message.author.id}', '${xptogive}', '0')`
         await executeQuery(queryAdd)
-
-    }
-
-    let level = parseInt(results[0].level)
-    let xp = parseInt(results[0].xp)
-    let xptogive = Math.floor(Math.random() * 30) + 15;
-
-    if((level + 1) * 1000 <= xp) {
-
-        const queryXpupdate = `UPDATE xp SET xp = '${xptogive}', level = '${level + 1}' WHERE guild = '${message.guildId}' AND user = '${message.author.id}'`
-        await executeQuery(queryXpupdate)
-
-        let channel = message.guild.channels.cache.get(process.env.LEVEL_PASS_CHANNEL);
-        channel.send(`Tu l'as fais ${message.author}, tu arrives au niveau ${level + 1}. Bien jouée à toi!`)
 
     } else {
 
-        const queryXpUpdate = `UPDATE xp SET xp = '${xp + xptogive}' WHERE guild = '${message.guildId}' AND user = '${message.author.id}'`
-        await executeQuery(queryXpUpdate)
+        let level = parseInt(results[0].level)
+        let xp = parseInt(results[0].xp)
 
+        if ((level + 1) * 1000 <= xp) {
+
+            const queryXpupdate = `UPDATE xp SET xp = '${xptogive}', level = '${level + 1}' WHERE guild = '${message.guildId}' AND user = '${message.author.id}'`
+            await executeQuery(queryXpupdate)
+
+            let channel = message.guild.channels.cache.get(process.env.LEVEL_PASS_CHANNEL);
+            channel.send(`Tu l'as fais ${message.author}, tu arrives au niveau ${level + 1}. Bien jouée à toi!`)
+
+        } else {
+
+            const queryXpUpdate = `UPDATE xp SET xp = '${xp + xptogive}' WHERE guild = '${message.guildId}' AND user = '${message.author.id}'`
+            await executeQuery(queryXpUpdate)
+
+        }
     }
 
     const channel = await bot.channels.fetch(process.env.COUNTING_CHANNEL);
