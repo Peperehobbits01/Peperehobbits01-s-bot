6e9e73a3861205fea63dc47b210f4636ae4115f4
Peperehobbits01 <peperehobbits01@proton.me>
2026-07-11 16:58:13 +0200

    Fix: Gateway events not sending in their own channel and also fixed some undefined issues.

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -5,12 +5,12 @@ module.exports = async (bot, member) => {
     const welcomeChannel = member.guild.channels.cache.get(process.env.WELCOME_CHANNEL)
     welcomeChannel.send(`Bienvenue à ${member}, il vient d'arrivée sur le serveur!`)
 
-    const logsChannel = member.guild.channels.cache.get(process.env.LOGS_CHANNEL_MEMBER)
+    const logsChannel = member.guild.channels.cache.get(process.env.LOGS_CHANNEL_GATEWAY)
 
     const logsNewMember = new Discord.EmbedBuilder()
         .setColor(process.env.BOT_COLOR)
         .setAuthor({name: member.displayName, iconURL: member.displayAvatarURL({dynamic : true})})
-        .setDescription(`${member} a rejoint le serveur.\nUtilisateur : ${member.name}\n**ID** :\nUtilisateur : ${member.id}`)
+        .setDescription(`${member} a rejoint le serveur.\nUtilisateur : ${member.displayName}\n**ID** :\nUtilisateur : ${member.id}`)
         .setFooter({ text: "Gérée par l'instance de Peperehobbits01's Bot", iconURL: bot.user.displayAvatarURL({ dynamic: true }) })
         .setTimestamp()
 
