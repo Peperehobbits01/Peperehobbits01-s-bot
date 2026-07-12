37e7bbf67e0e9e31188b095a4ac4fc7956f0e2ac
Peperehobbits01 <peperehobbits01@proton.me>
2026-07-12 14:16:04 +0200

    Fix: Refine validation and messaging in the note command

--- Commandes/Modération/note.js
+++ Commandes/Modération/note.js
@@ -27,17 +27,17 @@ module.exports = {
     async run(bot, message, args) {
 
         let user = args.getUser("membre")
-        if(!user) return message.reply("Pas de membre à notée!")
+        if(!user) return message.reply("Aucun membre a notée.")
         let member = message.guild.members.cache.get(user.id)
-        if(!member) return message.reply("Pas de membre à notée.")
+        if(!member) return message.reply("Aucun membre a notée.")
 
         let reason = args.getString("note")
-        if(!reason) reason = "Note manquante";
+        if(!reason) return message.reply("Note manquante.")
 
-        if(message.user.id === user.id) return message.reply("Tu ne peux pas te notée!")
-        if((await message.guild.fetchOwner()).id === user.id) return message.reply("Le fondateur ne peux pas être notée!")
-        if(member && !member.kickable) return message.reply("Je ne peux le notée!")
-        if(member && message.member.roles.highest.comparePositionTo(member.roles.highest) <= 0) return message.reply("Tu ne peux pas le notée!")
+        if(message.user.id === user.id) return message.reply("Tu ne peux pas te notée !")
+        if((await message.guild.fetchOwner()).id === user.id) return message.reply("Le fondateur ne peux pas être notée !")
+        if(member && !member.kickable) return message.reply("Je ne peux le notée !")
+        if(member && message.member.roles.highest.comparePositionTo(member.roles.highest) <= 0) return message.reply("Tu ne peux pas le notée !")
 
         await message.deferReply()
 
@@ -45,13 +45,15 @@ module.exports = {
         .setTitle("Informations de la note")
         .setDescription(`Vous avez mis une note à ${user.tag} et voici sa note : \`${reason}\` avec succès !`)
         .setColor(process.env.BOT_COLOR)
-        .setFooter({ text: "Gérée par l'instance de Peperehobbits01's Bot", iconURL: bot.user.displayAvatarURL({ dynamic: true }) })
+        .setFooter({
+            text: "Gérée par l'instance de Peperehobbits01's Bot",
+            iconURL: bot.user.displayAvatarURL({dynamic: true})
+        })
         await message.followUp({embeds: [Note1]})
 
         let ID = await bot.function.createId("NOTE")
  
         const queryNoteAdd = `INSERT INTO note (guild, user, author, note, reason, date) VALUES ('${message.guild.id}', '${user.id}', '${message.user.id}', '${ID}', '${reason.replace(/'/g, "\\'")}', '${Date.now()}')`
         await executeQuery(queryNoteAdd)
-        
     }
 }
